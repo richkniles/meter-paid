59c72a4d7ae241dd1dd254d8f6a2b0712d433e23
Matthew Du Pont <dupont@okcupidlabs.com>
2012-08-07 19:15:47 -0700

    test push

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -16,7 +16,7 @@ module SchedulesHelper
     @account.calls.create({
       :from => '+15106936860',#'+15109240329', #trial sandbox #
       :to => '+15106936860',
-      :url => 'http://floating-wind-2082.herokuapp.com/twilio',
+      :url => 'http://localhost:3000/twilio',
       :SendDigits => "#{format_extensions_and_delays(schedule, user)}"
     })
     
